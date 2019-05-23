a0484656914804052a6169177d1eb9cf065243af
Vadim Shalamov <vadshalamov@gmail.com>
2019-05-23 14:24:36 +0400

    f

--- modules/db_subnet_group/main.tf
+++ modules/db_subnet_group/main.tf
@@ -3,7 +3,7 @@ resource "aws_db_subnet_group" "this" {
 
   name_prefix = "${var.name_prefix}"
   description = "Database subnet group for ${var.identifier}"
-  subnet_ids  = ["${var.subnet_ids}"]
+  subnet_ids  = "${var.subnet_ids}"
 
   tags = "${merge(var.tags, map("Name", format("%s", var.identifier)))}"
 }
